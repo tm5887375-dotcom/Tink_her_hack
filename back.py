cc6c8deeae7af2cadd09df60fb2f91b3bd147f77
Algorithmics <tm5887375@gmail.com>
2026-02-28 09:05:10 +0530

    Changes for hosting

--- back.py
+++ back.py
@@ -1,6 +1,7 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 import sqlite3
+import os
 
 app = Flask(__name__)
 CORS(app)  # allow frontend connection
@@ -154,4 +155,5 @@ def get_all_jobs():
 if __name__ == "__main__":
     init_db()  # create tables automatically
     seed_jobs()
-    app.run(port=3000, debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
